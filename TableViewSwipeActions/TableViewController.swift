0247d680afec278530067277d34bd40837f0f487
leannelis <leannelis@users.noreply.github.com>
2020-05-08 16:32:16 -0500

    finalize demo code

--- TableViewSwipeActions/TableViewController.swift
+++ TableViewSwipeActions/TableViewController.swift
@@ -47,24 +47,23 @@ class TableViewController: UITableViewController {
         return cell
     }
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 	
-    /*
+	
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            // Delete the row from the data
+			tableData.remove(at: indexPath.row)
+			// Delete the row from the table itself
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
 }
